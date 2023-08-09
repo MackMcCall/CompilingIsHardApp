6f45dbd81c5335bfd2db1987d06da7af25683e35
Mack McCall <mackmc9924@gmail.com>
2023-08-09 14:24:55 -0600

    Achievement Unlocked: Bug Squasher

--- CompilingIsHardConsoleUI/Person.cs
+++ CompilingIsHardConsoleUI/Person.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CompilingIsHardConsoleUI
 {
-    internal static class Person
+    public class Person
     {
         public string Name { get; set; }
         public int Age { get; set; }
@@ -14,14 +14,13 @@ namespace CompilingIsHardConsoleUI
         public DateTime Dob { get; set; }
         public string EmailAddress { get; set; }
         public string PhoneNumber { get; set; }
-        public string Name { get; set; }
 
-        private int numberOfLegs = 0;
+        private static readonly int numberOfLegs = 0;
         public int NumberOfLegs { get; init; } = numberOfLegs;
 
-        public static void SayName()
+        public void SayName()
         {
-            Console.WriteLine($"My name is {this.Name}");
+            Console.WriteLine($"My name is {Name}");
         }
 
     }
--- CompilingIsHardConsoleUI/Program.cs
+++ CompilingIsHardConsoleUI/Program.cs
@@ -8,61 +8,71 @@ namespace CompilingIsHardConsoleUI
     {
         static int Main(string[] args)
         {
-            int exitCode;
+            int exitCode = 1;
+            
             Console.WriteLine("Hello!");
             Console.WriteLine("Please give me a number");
-            userNumber = Console.ReadLine();
+            int userNumber = int.Parse(Console.ReadLine());
+            
             Console.WriteLine("Please give me another number");
-            userNumber = Console.ReadLine();
-            answer = userNumber / userNumber;
-            Console.WriteLine("{userNumber} / {userNumber} = {answer}");
+            int secNumber = int.Parse(Console.ReadLine());
+            double answer = userNumber / secNumber;
+            Console.WriteLine($"{userNumber} / {secNumber} = {answer}");
+            
             Console.WriteLine("Please give me your age!");
-            const int USER_AGE = Console.ReadLine();
-            if (USER_AGE = 21)
+            int user_age = int.Parse(Console.ReadLine());
+            if (user_age == 21)
             {
                 Console.WriteLine("You may drink alcohol!");
             }
+            
             Console.WriteLine("Give me the name of everyone that lives in your house separated by commas");
             string[] names = Console.ReadLine().Split('\u002C');
             List<Person> people = new List<Person>();
-            foreach (Person name as name)
+            foreach (string name in names)
             {
                 Person p = new Person();
-                p.Name == name;
+                p.Name = name;
                 p.SayName();
-                Console.WriteLine("What is {name}'s date of birth?");
-                p.Dob = Console.ReadLine();
+                Console.WriteLine($"What is {name}'s date of birth?");
+                p.Dob = DateTime.Parse(Console.ReadLine());
                 people.Add(p);
             }
-            for (i = 0; i < 100; i++)
+            
+            for (int i = 0; i < 100; i++)
             {
                 Console.WriteLine(i);
             }
+            
             var something = DoSomething();
+            
             Console.WriteLine("What is your favorite color?");
-            switch (Console.ReadLine().ToUpper())
+            switch (Console.ReadLine().ToLower())
             {
                 case "red":
                     Console.WriteLine("Red is great!");
-                case: "blue":
+                    break;
+                case "blue":
                     Console.WriteLine("Blue is awesome!");
-                case: "yellow":
+                    break;
+                case "yellow":
                     Console.WriteLine("Yellow reminds me of the sun");
-                case: "orange":
+                    break;
+                case "orange":
                     Console.WriteLine("I like orange juice!");
-                case: "blue":
-                    Console.WriteLine("Blue is awesome!");
+                    break;
                 default:
-                    Console.WriteLine("Never heard of that color, what does it look like?");                    
+                    Console.WriteLine("Never heard of that color, what does it look like?");
+                    break;
             }
             try
             {
                 Random random = new Random();
-                if (random.Next(1,100) % 2 = 0)
+                if (random.Next(1,100) % 2 == 0)
                 {
-                    throw new NullReferenceException();
+                    throw new Exception();
                 }
-                else (if random.Next(1, 100) % 3 = 0)
+                else if (random.Next(1, 100) % 3 == 0)
                 {
                     throw new Exception();
                 }
@@ -75,33 +85,28 @@ namespace CompilingIsHardConsoleUI
             {
                 Console.WriteLine(e.Message);
                 return exitCode;
-                
-            }
-            catch (NullReferenceException e)
-            {
-                Console.WriteLine(e.Message);
             }
             finally
             {
                 Console.WriteLine("Henlooooo");
             }
             
-            var something = DoSomethingElse();
+            var somethingelse = DoSomethingElse();
 
             return exitCode;
         }
 
-        private void DoSomething()
+        private static int DoSomething()
         {
             Console.WriteLine("I am doing something!");
             return 15;
         }
 
-        private string DoSomethingElse()
+        private static string DoSomethingElse()
         {
-            Console.WriteLine("I am doing something else!");            
+            return "I am doing something else!";
         }
-        static void Main()
+        static void Garbage()
         {
             Console.WriteLine("Blah Blah Blah");
         }
